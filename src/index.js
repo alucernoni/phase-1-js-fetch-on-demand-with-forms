f7dbdd5299aaf0d70508e004a3c08004c84685d3
Annemarie <a.lucernoni0@gmail.com>
2022-05-03 21:47:35 -0700

    commit tree 8daf604dd84b159b7c4d52f24db722dea7241e6f

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
+  const inputForm = document.querySelector('form')
   
+  inputForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      const inputValue = document.querySelector('#searchByID').value
+      inputForm.reset()
+      
+
+    fetch(`http://localhost:3000/movies/${inputValue}`)
+    .then(resp => resp.json())
+    .then(data => {
+        const title = document.querySelector('#movieDetails h4')
+        const summary = document.querySelector('#movieDetails p')
+
+        title.textContent = data.title
+        summary.textContent = data.summary
+
+        })
+    })
 }
 
 document.addEventListener('DOMContentLoaded', init);
